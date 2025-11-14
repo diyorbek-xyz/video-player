2fa46d5729d9eb761cdfcb288db8daceac594b4f
diyorbek.xyz <anymouslypc@gmail.com>
2025-11-14 14:05:40 +0500

    gg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { TimeRange, PlayPause, Mute, VolumeControls, Barrier, Status, TimeView, Download, Fullscreen, Resolutions, SkipMobile } from './video-player/controllers';
 import Player, { PlayerContextProvider, usePlayerContext } from './video-player/player';
-import { SectionControls, SectionOther, SectionVolume } from './video-player/player_sections';
+import { ControlsTop, ControlsBottom, SectionOverlay, SectionOther, SectionVolume } from './video-player/player_sections';
 
 export { PlayerContextProvider, usePlayerContext };
-export { SectionControls, SectionOther, SectionVolume };
+export { ControlsTop, ControlsBottom, SectionOverlay, SectionOther, SectionVolume };
 export { TimeRange, PlayPause, Mute, VolumeControls, Barrier, Status, TimeView, Download, Fullscreen, Resolutions, SkipMobile };
 export default Player;
